19830018d6a42355b3bf3fee9f487f266ac83aaa
Verbosi7y <xuedarwin@gmail.com>
2024-04-26 00:42:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,13 +42,16 @@ if 'img1' not in st.session_state:
 
 st.title("Classifying Alzheimer's Disease")
 st.write("""
-Alzheimer's Disease is classified in terms of dementia severity.
-Pick a random image below to model the dementia rating.
-""")
+        *Alzheimer's Disease* is classified in terms of dementia severity.\n
+        Pick a random image below to model the dementia rating.\n
+        **Note:** The model does not know the actual severity.
+        Only ***YOU*** know the classification labeled on the image captions. The model will try to predict the caption/label/classification.
+        """)
 
 img = image_select(
-    label="Select an MRI scan of the brain",
-    images=[st.session_state.img1, st.session_state.img2, st.session_state.img3, st.session_state.img4]
+    label="**Select an MRI scan of the brain**",
+    images=[st.session_state.img1, st.session_state.img2, st.session_state.img3, st.session_state.img4],
+    captions=["Non Demented", "Very Mild Demented", "Mild Demented" , "Moderate Demented"]
 )
 
 if st.button("Predict Dementia Severity"):
@@ -56,5 +59,14 @@ if st.button("Predict Dementia Severity"):
     with st.spinner("Predicting..."):
         result = predict(img)
 
-    st.write(label_mapping(result))
+    st.write("Prediction: ", label_mapping(result))
     st.image(img)
+
+
+st.write("""
+        ### Evaluation Metrics based on model used:\n
+        Non_Demented(0) - Accuracy: 0.9421875, Precision: 0.9435736677115988, Recall: 0.940625
+        Very_Mild_Demented(1) - Accuracy: 0.9359375, Precision: 0.9103139013452914, Recall: 0.90625
+        Mild_Demented(2) - Accuracy: 0.9671875, Precision: 0.8743169398907104, Recall: 0.8938547486033519
+        Moderate_Demented(3) - Accuracy: 1.0, Precision: 1.0, Recall: 1.0
+        """)
