e483edb60a1b85d0dbf7bb73a637297d2c2ef0be
Verbosi7y <xuedarwin@gmail.com>
2024-04-25 22:30:36 -0400

    added package requirements, streamlit + image selection and prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,50 @@
 import streamlit as st
+from streamlit_image_select import image_select
+
+import torch
+import numpy as np
+
+from PIL import Image
+
+import os, random
 from alzheimersdetection import AlzheimerModel
 
 def predict(data):
-    labels = ["Non_Demented",
-              "Very_Mild_Demented",
-              "Mild_Demented",
-              "Moderate_Demented"]
+
     
-    clf = AlzheimerModel.load(fr'models\best_ad_model.pt')
+    model = AlzheimerModel.load(fr'models\best_ad_model.pt')
+    result = AlzheimerModel.predict(model, data)
+
+    return result
+
+def label_mapping(result):
+    labels = ["Not Demented",
+              "Very Mild Demented",
+              "Mild Demented",
+              "Moderate Demented"]
+    
+    return labels[result]
+
+img1 = np.array(Image.open(random.choice(os.listdir(fr'assets\raw\Non_Demented'))))
+img2 = np.array(Image.open(random.choice(os.listdir(fr'assets\raw\Very_Mild_Demented'))))
+img3 = np.array(Image.open(random.choice(os.listdir(fr'assets\raw\Mild_Demented'))))
+img4 = np.array(Image.open(random.choice(os.listdir(fr'assets\raw\Moderate_Demented'))))
+
+st.title("Classifying Alzheimer's Disease")
+st.write("""
+Alzheimer's Disease is classified in terms of dementia severity.
+Pick a random image below to model the dementia rating.
+""")
+
+img = image_select(
+    label="Select an MRI scan of the brain",
+    images=[img1, img2, img3, img4]
+)
+
+if st.button("Predict Dementia Severity"):
+    result = None
+    with st.spinner("Predicting..."):
+        result = predict(img)
+
+    st.write(label_mapping(result))
+    st.image(img)
